8e6a19a68bdbd979d4018eef806cc179fbe8743c
Axel Olivecrona <olivecrona.axel@gmail.com>
2023-08-24 11:01:56 +0200

    first commit new app

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shopping_app/constants.dart';
+
+// stl för att bygga något snabbt
+
+class HomeScreen extends StatelessWidget {
+  // const HomeScreen({super.key});   Vet ej om denna behövs
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/back.svg"), onPressed: () {}),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            color: kTextColor,
+          ),
+          // color: kTextColor,
+          onPressed: () {},
+        ),
+        IconButton(
+          // color: Colors.black87,
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(
+          width: kDefaultPaddin / 2,
+        )
+      ],
+    );
+  }
+}
